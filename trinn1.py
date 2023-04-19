d426e8baf69c99c5834d23e4d2ae5bd204848300
chso1234 <111433404+chso1234@users.noreply.github.com>
2023-04-19 14:11:36 +0200

    0

---
+++ trinn1.py
@@ -0,0 +1,20 @@
+# Definer variabler
+a = 10
+b = 5
+
+# Utfør grunnleggende aritmetikk og lagre resultatene i nye variabler
+addisjon = a + b
+subtraksjon = a - b
+multiplikasjon = a * b
+divisjon = a / b
+
+# Skriv ut resultatene ved hjelp av print() funksjonen
+print("Addisjon:", addisjon)
+print("Subtraksjon:", subtraksjon)
+print("Multiplikasjon:", multiplikasjon)
+print("Divisjon:", divisjon)
+
+Addisjon: 20
+Subtraksjon: 0
+Multiplikasjon: 100
+Divisjon: 1.0
